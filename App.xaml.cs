c4214bc77aeac2e3b6c47ded2874169a4d83647d
Chase Swick <chase.swick@mandsconsulting.com>
2025-01-02 13:31:40 -0500

    Initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace Maui_Test_App
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Maui_Test_App
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
